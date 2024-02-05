fd30c49e24b5ed011f07a1244240ec890b808341
yash261260 <157184669+sola0050@users.noreply.github.com>
2024-02-05 00:00:55 -0500

    commit

--- app/api-services/open-weather-service.ts
+++ app/api-services/open-weather-service.ts
@@ -1,46 +1,26 @@
-const API_KEY = process.env.WEATHER_API_KEY
-const TEN_MINUTES = 1000 * 60 * 10 // in milliseconds
+import { redis } from '../data-access/redis-connection'
 
-const resultsCache: Record<string, {lastFetch: number; data: unknown}> = {}
-function getCacheEntry(key: string) {
-  return resultsCache[key]
-}
-function setCacheEntry(key: string, data: unknown) {
-  resultsCache[key] = {lastFetch: Date.now(), data}
-}
-function isDataStale(lastFetch: number) {
-  return Date.now() - lastFetch > TEN_MINUTES
-}
+const API_KEY = process.env.WEATHER_API_KEY
+const BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall'
+const TEN_MINUTES = 1000 * 60 * 10 // in milliseconds
 
 interface FetchWeatherDataParams {
   lat: number
   lon: number
-  units: string
+  units: 'standard' | 'metric' | 'imperial'
 }
 export async function fetchWeatherData({
   lat,
   lon,
   units
 }: FetchWeatherDataParams) {
-  const baseURL = 'https://api.openweathermap.org/data/3.0/onecall'
-  const queryString = `lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`
+  const queryString = `lat=${lat}&lon=${lon}&units=${units}`
 
-  const cacheEntry = getCacheEntry(queryString)
-  if (cacheEntry && !isDataStale(cacheEntry.lastFetch)) {
-    return cacheEntry.data
-  }
-  const response = await fetch(`${baseURL}?${queryString}`)
-  const data = await response.json()
-  setCacheEntry(queryString, data)
-  return data
-}
+  const cacheEntry = await redis.get(queryString)
+  if (cacheEntry) return JSON.parse(cacheEntry)
 
-export async function getGeoCoordsForPostalCode(
-  postalCode: string,
-  countryCode: string
-) {
-  const url = `http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},${countryCode}&appid=${API_KEY}`
-  const response = await fetch(url)
-  const data = response.json()
-  return data
+  const response = await fetch(`${BASE_URL}?${queryString}&appid=${API_KEY}`)
+  const data = await response.text() // avoid an unnecessary extra JSON.stringify
+  await redis.set(queryString, data, {PX: TEN_MINUTES}) // The PX option sets the expiry time
+  return JSON.parse(data)
 }
--- app/data-access/redis-connection.ts
+++ app/data-access/redis-connection.ts
@@ -1,34 +1,7 @@
 import {createClient} from 'redis'
-import { redis } from '../data-access/redis-connection'
 
 const url = process.env.REDIS_URL || 'redis://localhost:6379'
 
 export const redis = await createClient({url})
   .on('error', err => console.error('Redis client connection error', err))
   .connect()
-
-
-  const API_KEY = process.env.WEATHER_API_KEY
-  const BASE_URL = 'https://api.openweathermap.org/data/3.0/onecall'
-  const TEN_MINUTES = 1000 * 60 * 10 // in milliseconds
-  
-  interface FetchWeatherDataParams {
-    lat: number
-    lon: number
-    units: 'standard' | 'metric' | 'imperial'
-  }
-  export async function fetchWeatherData({
-    lat,
-    lon,
-    units
-  }: FetchWeatherDataParams) {
-    const queryString = `lat=${lat}&lon=${lon}&units=${units}`
-  
-    const cacheEntry = await redis.get(queryString)
-    if (cacheEntry) return JSON.parse(cacheEntry)
-  
-    const response = await fetch(`${BASE_URL}?${queryString}&appid=${API_KEY}`)
-    const data = await response.text() // avoid an unnecessary extra JSON.stringify
-    await redis.set(queryString, data, {PX: TEN_MINUTES}) // The PX option sets the expiry time
-    return JSON.parse(data)
-  }
